3657f472eafad1ff615873d3c4de03a80102780d
Herdex <66902036+Herdex@users.noreply.github.com>
2023-06-21 13:41:24 -0300

    Update modinfo.lua

--- mods/perry/modinfo.lua
+++ mods/perry/modinfo.lua
@@ -1,7 +1,7 @@
 -- This information tells other players more about the mod
 name = "Perry"
-description = "A character template for Don't Starve Together."
-author = "Dragon Wolf Leo"
+description = "One of the Pepega Pals."
+author = "Me"
 version = "1.2.6" -- This is the version of the template. Change it to your own number.
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
